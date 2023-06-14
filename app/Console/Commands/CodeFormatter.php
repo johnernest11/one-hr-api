63f92bc6b0f090d66ebc54fbacc116209fae25f0
jegramos <jegramos.pa@gmail.com>
2023-06-14 11:33:37 +0800

    :bug: window potential fix

--- app/Console/Commands/CodeFormatter.php
+++ app/Console/Commands/CodeFormatter.php
@@ -33,7 +33,7 @@ class CodeFormatter extends Command
     public function handle(): int
     {
         $pintTestArg = $this->option('test') ? '--test' : '';
-        $pintCommand = 'vendor/bin/pint '.$pintTestArg;
+        $pintCommand = './vendor/bin/pint '.$pintTestArg;
 
         $this->info("\u{1F9F9} Cleaning up your dirty code...");
         $exitCode = Command::SUCCESS;
