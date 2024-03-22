2dfc9ce309ed72357fe67d4a595fd3e97823fd4e
jegramos <jegramos.pa@gmail.com>
2024-03-22 15:29:11 +0800

    :memo: Removed docs dir, added welcome page, added docs page

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome-page');
+
+Route::get('/docs', function () {
+    return view('docs');
+})->name('docs-page');
