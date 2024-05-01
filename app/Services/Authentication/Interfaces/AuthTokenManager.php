9b3cae26af17c663c17b66e41399081ce240da9e
jegramos <jegramos.pa@gmail.com>
2024-05-01 13:45:05 +0800

    :arrow_up: Upgrade composer requirements to PHP8.2

--- app/Services/Authentication/Interfaces/AuthTokenManager.php
+++ app/Services/Authentication/Interfaces/AuthTokenManager.php
@@ -10,8 +10,8 @@ interface AuthTokenManager
     /**
      * Generate an auth token for the user
      *
-     * @param  User  $user - The token will be generated for a specific user
-     * @param  string  $clientName - Clients (SPA, mobile) can optionally send 'My iPhone14', 'Google Chrome', etc
+     * @param  User  $user  - The token will be generated for a specific user
+     * @param  string  $clientName  - Clients (SPA, mobile) can optionally send 'My iPhone14', 'Google Chrome', etc
      */
     public function generateToken(User $user, Carbon $expiresAt, string $clientName = ''): string;
 
