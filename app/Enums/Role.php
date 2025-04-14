23315c78e25f8a27f266973cafd0d7724caf096c
Marianne Cyra Jun R. Quinzon <mcjrquinzon@ENTDSWD.LOCAL>
2025-04-14 11:34:40 +0800

    Added section and division head as roles.

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -8,6 +8,8 @@ enum Role: string
     case ADMIN = 'admin';
     case HR_PAS_ADMIN = 'hr_pas_admin';
     case HR_PPMS_ADMIN = 'hr_ppms_admin';
+    case SECTION_HEAD = 'section_head';
+    case DIVISION_HEAD = 'division_head';
     case SYSTEM_SUPPORT = 'system_support';
     case SUPER_USER = 'super_user';
 }
