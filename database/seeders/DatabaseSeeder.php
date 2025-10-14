5a7296fc3ac094da1bc8b47777b87d7a44d07981
Marianne Cyra Jun R. Quinzon <mcjrquinzon@ENTDSWD.LOCAL>
2025-10-14 11:37:36 +0800

    Updated the seeder such that an app:init is not required anymore.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,6 +35,7 @@ class DatabaseSeeder extends Seeder
         CountriesSeeder::class,
         SalaryGradesSeeder::class,
         ProgramsSeeder::class,
+        LocatorSlipPermissionSeeder::class,
     ];
 
     /**
