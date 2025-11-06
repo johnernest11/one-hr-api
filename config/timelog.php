9d6771b8f5e7050301aeffb0c40b7bdc2b9b5ee6
Marianne Cyra Jun R. Quinzon <mcjrquinzon@ENTDSWD.LOCAL>
2025-11-06 11:46:19 +0800

    Fixed bugs related to time logs service: - Fixed issue wherein during time log creation, the corresponding   locator slip logger is not being updated. - Fixed issue wherein during time log creation, the scanned time is also   capturing the seconds. - DUPLICATE_SCAN_LIMIT_MINUTES is now an environmental variable.

---
+++ config/timelog.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | All configurations related to Time Logs
+     |--------------------------------------------------------------------------
+     | This contains all configurations related to the time logs and related modules.
+     |
+     */
+    'duplicate_scan_limit' => env('DUPLICATE_SCAN_LIMIT_MINUTES', 15),
+];
