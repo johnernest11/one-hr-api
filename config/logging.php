ef07a096d6586361fceff68c6ccd2a079138e518
Marianne Cyra Jun R. Quinzon <mcjrquinzon@dswd.gov.ph>
2026-08-21 11:53:13 +0800

    Added read & write permission to owner and group, and read only to others.
    This is to resolve permission denied issue when generating time logs.

--- config/logging.php
+++ config/logging.php
@@ -69,6 +69,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 30,
+            'permission' => 0664,
         ],
 
         'slack' => [
