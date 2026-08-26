331546d8e6a6640c7a844b43bde838dc57c13de1
Marianne Cyra Jun R. Quinzon <mcjrquinzon@dswd.gov.ph>
2026-08-26 08:54:33 +0800

    Temporary change: Added Logs to debug AWS issue in staging

--- app/Traits/Controllers/CanMoveCapturedImageToCloud.php
+++ app/Traits/Controllers/CanMoveCapturedImageToCloud.php
@@ -19,27 +19,53 @@ trait CanMoveCapturedImageToCloud
         CloudStorageManager $cloudStorage,
         int|string|null $employeeId = null
     ): ?string {
-        try {
-            $folder = 'images/timelogs/'.now()->format('Y-m-d');
-            if ($employeeId) {
-                $folder .= '/'.$employeeId;
-            }
+        // try {
+        $folder = 'images/timelogs/'.now()->format('Y-m-d');
+        if ($employeeId) {
+            $folder .= '/'.$employeeId;
+        }
 
-            $extension = $file->getClientOriginalExtension()
-                ?: pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION)
-                ?: 'jpg';
+        Log::info('DEBUG [moveCapturedImageToCloud]: Starting upload...', [
+            'file_original_name' => $file->getClientOriginalName(),
+            'file_mime_type' => $file->getMimeType(),
+            'file_size_bytes' => $file->getSize(),
+            'file_is_valid' => $file->isValid(),
+            'file_error_code' => $file->getError(),
+            'employee_id' => $employeeId,
+        ]);
 
-            $fileName = 'log_'.now()->timestamp.'_'.Str::random(8).'.'.$extension;
-
-            $fullPath = $cloudStorage->upload($folder, $file, $fileName);
-
-            return $fullPath;
-        } catch (\Exception $e) {
-            Log::error('Failed to upload captured image: '.$e->getMessage(), [
-                'exception' => $e,
+        if (! $file->isValid()) {
+            Log::warning('DEBUG [moveCapturedImageToCloud]: File is invalid or upload limit exceeded!', [
+                'error_code' => $file->getError(),
             ]);
 
             return null;
         }
+
+        $extension = $file->getClientOriginalExtension()
+            ?: pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION)
+            ?: 'jpg';
+
+        $fileName = 'log_'.now()->timestamp.'_'.Str::random(8).'.'.$extension;
+
+        Log::info('DEBUG [moveCapturedImageToCloud]: Calling $cloudStorage->upload()', [
+            'target_folder' => $folder,
+            'target_file' => $fileName,
+        ]);
+
+        $fullPath = $cloudStorage->upload($folder, $file, $fileName);
+
+        Log::info('DEBUG [moveCapturedImageToCloud]: Upload completed successfully', [
+            'returned_full_path' => $fullPath,
+        ]);
+
+        return $fullPath;
+        // } catch (\Exception $e) {
+        // Log::error('Failed to upload captured image: '.$e->getMessage(), [
+        // 'exception' => $e,
+        // ]);
+
+        // return null;
+        // }
     }
 }
